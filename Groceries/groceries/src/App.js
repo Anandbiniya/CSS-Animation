cdf8712ec7f307c8d11c258c6e5f494f3b89f6bf
anandamith <96160297+AnandBiniya@users.noreply.github.com>
2023-01-06 21:44:15 +0530

    react

---
+++ Groceries/groceries/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { GroceryDetails } from "./components/GroceryDetails";
+
+function App() {
+  return (
+    <div className="App">
+      <GroceryDetails />
+    </div>
+  );
+}
+
+export default App;
---
+++ Groceries/groceries/src/components/GroceryDetails.js
@@ -0,0 +1,102 @@
+import React from "react";
+import Pro1 from "../Images/product1.jpg";
+import style from "./grocerrydetails.module.css";
+export const GroceryDetails = () => {
+  return (
+    <div>
+      <h1 className={style.groceryhead}>Groceries</h1>
+      <div data-cy="container" className={style.container}>
+        <div className={style.grocery_card}>
+          <img src={Pro1} />
+          <h4>Pears Pure & Gentle Body Wash 250 ml</h4>
+          <p>₹ 115.00</p>
+          <p>M.R.P: ₹ 135.00</p>
+          <button>
+            Add to cart <span>+</span>
+          </button>
+        </div>
+        <div className={style.grocery_card}>
+          <img src={Pro1} />
+          <h4>Pears Pure & Gentle Body Wash 250 ml</h4>
+          <p>₹ 115.00</p>
+          <p>M.R.P: ₹ 135.00</p>
+          <button>
+            Add to cart <span>+</span>
+          </button>
+        </div>
+        <div className={style.grocery_card}>
+          <img src={Pro1} />
+          <h4>Pears Pure & Gentle Body Wash 250 ml</h4>
+          <p>₹ 115.00</p>
+          <p>M.R.P: ₹ 135.00</p>
+          <button>
+            Add to cart <span>+</span>
+          </button>
+        </div>
+        <div className={style.grocery_card}>
+          <img src={Pro1} />
+          <h4>Pears Pure & Gentle Body Wash 250 ml</h4>
+          <p>₹ 115.00</p>
+          <p>M.R.P: ₹ 135.00</p>
+          <button>
+            Add to cart <span>+</span>
+          </button>
+        </div>
+        <div className={style.grocery_card}>
+          <img src={Pro1} />
+          <h4>Pears Pure & Gentle Body Wash 250 ml</h4>
+          <p>₹ 115.00</p>
+          <p>M.R.P: ₹ 135.00</p>
+          <button>
+            Add to cart <span>+</span>
+          </button>
+        </div>
+        <div className={style.grocery_card}>
+          <img src={Pro1} />
+          <h4>Pears Pure & Gentle Body Wash 250 ml</h4>
+          <p>₹ 115.00</p>
+          <p>M.R.P: ₹ 135.00</p>
+          <button>
+            Add to cart <span>+</span>
+          </button>
+        </div>
+        <div className={style.grocery_card}>
+          <img src={Pro1} />
+          <h4>Pears Pure & Gentle Body Wash 250 ml</h4>
+          <p>₹ 115.00</p>
+          <p>M.R.P: ₹ 135.00</p>
+          <button>
+            Add to cart <span>+</span>
+          </button>
+        </div>
+        <div className={style.grocery_card}>
+          <img src={Pro1} />
+          <h4>Pears Pure & Gentle Body Wash 250 ml</h4>
+          <p>₹ 115.00</p>
+          <p>M.R.P: ₹ 135.00</p>
+          <button>
+            Add to cart <span>+</span>
+          </button>
+        </div>
+        <div className={style.grocery_card}>
+          <img src={Pro1} />
+          <h4>Pears Pure & Gentle Body Wash 250 ml</h4>
+          <p>₹ 115.00</p>
+          <p>M.R.P: ₹ 135.00</p>
+          <button>
+            Add to cart <span>+</span>
+          </button>
+        </div>
+        <div className={style.grocery_card}>
+          <img src={Pro1} />
+          <h4>Pears Pure & Gentle Body Wash 250 ml</h4>
+          <p>₹ 115.00</p>
+          <p>M.R.P: ₹ 135.00</p>
+          <button>
+            Add to cart <span>+</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
